640aeb708dbb76e54292eb6bd4facfe1a5acebfd
Gergely Szilagyi <esziger@gmail.com>
2017-08-02 13:29:18 +0200

    Finished chater State and lifecycle
    https://facebook.github.io/react/docs/state-and-lifecycle.html
    
    Added clock implementation.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,45 @@ class WelcomeClass extends React.Component {
   }
 }
 
+
+class Clock extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  //this is a lifecycle hook, runs after the component output has been rendered
+  componentDidMount() {
+    //setup a timer in the browser to call tick() once a second
+    this.timerId = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    //React merges the object I provide to the current state
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+
+  }
+
+}
+
 class App extends Component {
   render() {
     
@@ -82,6 +121,7 @@ class App extends Component {
         <Welcome name="Gergely"/>
         <WelcomeClass/>
         <Comment author={author}/>
+        <Clock/>
       </div>
     );
   }
